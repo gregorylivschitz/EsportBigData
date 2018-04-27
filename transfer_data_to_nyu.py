c3136dc8db861764727471f578990880df376d70
mrpoor <greg15001@gmail.com>
2018-04-27 08:16:13 -0400

    Fixed up transfer

--- transfer_data_to_nyu.py
+++ transfer_data_to_nyu.py
@@ -9,11 +9,11 @@ def transfer_to(transfer_from, transfer_to):
     transfer_nyu = TransferToNYUCluster()
     if not os.path.exists(transfer_from):
         os.makedirs(transfer_from)
-        data_files = os.listdir(transfer_from)
+    data_files = os.listdir(transfer_from)
 
     for data_file in data_files:
-        transfer_from = os.path.join(transfer_from, '{}'.format(data_file))
-        transfer_nyu.transfer(transfer_from, transfer_to)
+        transfer_from_file = os.path.join(transfer_from, '{}'.format(data_file))
+        transfer_nyu.transfer(transfer_from_file, transfer_to)
     clean_up(transfer_from)
 
 def clean_up(transfer_from):
